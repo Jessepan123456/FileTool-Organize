35a2a0b5a3763aa53a2a08505ed7bd2ed8bd19b6
Jesse <Jessepan123456@gmail.com>
2026-05-18 22:19:15 -0600

    Grouping files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use std::fs::{create_dir, create_dir_all};
-use std::fs::{read_dir, rename};
+use std::collections::HashMap;
+use std::fs::{create_dir, create_dir_all, read_dir, rename};
 use std::io::Error;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Error> {
     //Folder Path Start
-    let folder_path = Path::new("../weather/src/pages");
+    let folder_path = Path::new("../../../downloads");
+    let mut file_groups: HashMap<String, Vec<PathBuf>> = HashMap::new();
+
+    // 1. Create target folder
+    // let new_folder = folder_path.join("RustPages");
+    // create_dir(&new_folder)?;
 
     //Opens the Folder and explore what in it
     for entry in read_dir(folder_path)? {
@@ -14,25 +19,68 @@ fn main() -> Result<(), Error> {
 
         //Print it out with it path
         if path.is_file() {
-            println!("Found File: {:?}", path);
+            files(path, &mut file_groups);
         } else {
-            println!("Found Folder: {:?}", path)
+            //Do Something with the folders IDK What yet
+            // println!("Found Folder: {:?}", path)
         }
 
-        if let Some(types) = path.extension().and_then(|e| e.to_str()) {
-            let result = file_type(types);
-            println!("{}", result);
-        }
+        // // 2. Build new file path
+        // //Takes the file name of path
+        // let file_name = path.file_name().unwrap();
+        // //Combine it with new folder that was made
+        // let new_path = new_folder.join(file_name);
+
+        // // 3. Move file
+        // //Move it
+        // rename(&path, &new_path)?;
+
+        // if let Some(types) = path.extension().and_then(|e| e.to_str()) {
+        //     let result = file_type(types);
+        //     println!("{}", result);
+        // }
     }
 
+    if Path::new("../../../Videos").exists() {
+        println!("Videos Folder exists")
+    } else if Path::new("../../../Images").exists() {
+        println!("Images Folder exists")
+    } else if Path::new("../../../Music").exists() {
+        println!("Music Folder exists")
+    } else if Path::new("../../../Documents").exists() {
+        println!("Images Folder exists")
+    } else if Path::new("../../../Code").exists() {
+        println!("Code Folder exists")
+    } else if Path::new("../../../Archives").exists() {
+        println!("Archives Folder exists")
+    } else {
+        println!("Some Folder are missing")
+    }
+
+    // println!("{:?}", file_groups);
     Ok(())
 }
 
-//File Type Helper Method
+//Helper Method for determining File Types
 fn file_type(types: &str) -> &str {
     match types {
-        "rs" => "Rust File",
+        "png" | "jpg" | "jpeg" | "gif" => "Images",
+        "mp4" | "mov" => "Videos",
+        "mp3" | "wav" => "Audio",
+        "pdf" | "docx" => "Documents",
+        "rs" | "js" | "java" => "Code",
+        "zip" | "rar" => "Archives",
         _ => "Other Unknown File",
+    }
+}
+
+//Helper Method for Grouping Files Types
+fn files(path: PathBuf, groups: &mut HashMap<String, Vec<PathBuf>>) {
+    if let Some(types) = path.extension().and_then(|e| e.to_str()) {
+        groups
+            .entry(file_type(types).to_string())
+            .or_default()
+            .push(path);
     }
 }
 
